393c1968156cebfd80467425f309ac0de98c77de
Carlos Jesus <c.sipiran07@gmail.com>
2021-03-30 23:26:37 -0300

    update of filter detail

--- dress-carlosjesus/src/components/ItemDetailContainer.jsx
+++ dress-carlosjesus/src/components/ItemDetailContainer.jsx
@@ -5,12 +5,10 @@ import {itemJson} from './itemListContainer'
 
 
 const getItems = (id) =>{
-    const detail = itemJson.filter(obj=> obj.id === id)
+    
     return new Promise(resolve=>{
         setTimeout(()=>{
-            //console.log('SOY ID :',id)
-            resolve(detail)
-            //console.log('HOLAAA',detail);
+            resolve(itemJson)
             /* id : 2,
             title : ' Campera Hoodie de hombre',
             img : 'https://http2.mlstatic.com/D_NQ_NP_908376-MLA43772884685_102020-O.webp',
@@ -23,11 +21,11 @@ const getItems = (id) =>{
 const ItemDetailContainer = ( ) => {
     const [item, setItem] = useState([]);
     const {id} = useParams();
-    //console.log(id);
+
     useEffect(()=>{
         getItems(Number(id))
         .then((res) => {
-            setItem(res)
+            setItem(res.filter(obj => obj.id === id))
         });
             
     },[id])
